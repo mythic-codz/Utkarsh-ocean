be593de7f32e3174ae136823a36964a6a7c9fce2
mythic-codz <shashanksv100@gmail.com>
2026-01-08 14:04:08 +0530

    Create page.tsx

---
+++ blank/page.tsx
@@ -0,0 +1,20 @@
+export default function Blank() {
+  return (
+    <div
+      style={{
+        background: "#000",
+        color: "#ff4444",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        fontSize: "26px",
+        fontFamily: "Arial",
+        textAlign: "center",
+      }}
+    >
+      🚫 DevTools Detected<br /><br />
+      Access Blocked
+    </div>
+  );
+}
